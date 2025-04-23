5477771e65341b69eceb9e149f330304f3e04f17
Jeffrey Duivenvoorden <jeffrey.duivenvoorden@allunited.nl>
2025-04-23 13:27:41 +0200

    Remove second bakery

--- bin/upgrade-bakery.sh
+++ bin/upgrade-bakery.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 # Pull latest bakery repo & re-run setup
-cd /srv/bakery/bakery
+cd /srv/bakery/
 sudo git pull
 sudo bash setup-bakery.sh
 echo "🔄 Bakery platform upgraded!"
