9f2bfd14bc07332d7515a76f0d0792668410ddb5
Jeffrey Duivenvoorden <jeffrey.duivenvoorden@allunited.nl>
2025-04-23 13:32:06 +0200

    use bakery config

--- bin/upgrade-bakery.sh
+++ bin/upgrade-bakery.sh
@@ -3,5 +3,6 @@ set -e
 # Pull latest bakery repo & re-run setup
 cd /srv/bakery/
 sudo git reset --hard origin/main
+source /etc/bakery/config
 sudo bash setup-bakery.sh
 echo "🔄 Bakery platform upgraded!"
