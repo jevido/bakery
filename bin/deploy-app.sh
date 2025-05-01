299cc5a9ea2c81933ec7e48fd25029d7ca378842
Jeffrey Duivenvoorden <jheremenis@gmail.com>
2025-05-01 15:24:22 +0200

    Use variable i guess

--- bin/deploy-app.sh
+++ bin/deploy-app.sh
@@ -90,7 +90,7 @@ sudo -u bakery cp "$APP_DIR/.env" "$CURRENT/.env"
 sudo -u bakery bash -lc "
   cd $CURRENT
   bun install
-  bun --bun --env-file=.env run build
+  ORIGIN=$SUB bun --bun run build
 "
 
 if sudo -u bakery bash -lc "cd $CURRENT && bun run" | grep -a 'db:push'; then
