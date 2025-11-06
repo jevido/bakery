b562550e98b72452c62826248946d1754a9e67db
jevido <jheremenis@gmail.com>
2025-11-06 18:16:15 +0100

    try something

--- scripts/bootstrap-control-plane.sh
+++ scripts/bootstrap-control-plane.sh
@@ -219,6 +219,7 @@ if [[ ! -d "$INSTALL_DIR/.git" ]]; then
     echo "[debug] cloning into $INSTALL_NAME from \$(pwd)"
   fi
   git clone "$REPO_URL" "$INSTALL_NAME"
+  cd "$INSTALL_NAME"
 else
   cd "$INSTALL_DIR"
   git remote set-url origin "$REPO_URL"
